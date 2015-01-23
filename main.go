51693da4e5777522ac800f6b874ab51641580f15
William Hollacsek <william.hollacsek@gmail.com>
2015-01-24 00:19:00 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	doc, err := goquery.NewDocument("http://itinuae.com/torrents/movies/")
+	if err != nil {
+		fmt.Println("goquery:", err)
+	}
+
+	var fields []string
+
+	// Find table header and determine available fields
+	doc.Find("tr th").Each(func(i int, s *goquery.Selection) {
+		text := strings.TrimSpace(s.Text())
+
+		if s.Children().Is("img") {
+			fields = append(fields, "img")
+		} else if text != "" {
+			fields = append(fields, text)
+		}
+	})
+
+	var items []map[string]string
+
+	// Run through each row and extract data
+	doc.Find("tr").Each(func(i int, s *goquery.Selection) {
+		tds := s.Find("td")
+
+		// Row is empty
+		if text := strings.TrimSpace(tds.Text()); text == "" {
+			return
+		}
+
+		// Row has incorrect structure
+		if tds.Size() != len(fields) {
+			return
+		}
+
+		// Row has correct structure
+		data := map[string]string{}
+		tds.Each(func(i int, s *goquery.Selection) {
+			// Ignore the img field
+			if fields[i] == "img" {
+				return
+			}
+
+			data[fields[i]] = s.Text()
+		})
+		items = append(items, data)
+
+		fmt.Println(data)
+		fmt.Println("=============")
+	})
+}
